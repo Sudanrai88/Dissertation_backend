1ad7fbfcdd5ec892187e14382a8d8bcd772c7e42
Sudanrai88 <101595305+Sudanrai88@users.noreply.github.com>
2023-07-19 23:05:54 +0100

    Algorithm comments

--- src/main/java/com/example/demo/services/Algorithms.java
+++ src/main/java/com/example/demo/services/Algorithms.java
@@ -7,8 +7,6 @@ import com.google.cloud.firestore.Firestore;
 import com.google.cloud.firestore.WriteResult;
 import com.google.firebase.cloud.FirestoreClient;
 import org.springframework.stereotype.Service;
-
-import java.util.Comparator;
 import java.util.List;
 import java.util.concurrent.ExecutionException;
 
@@ -18,11 +16,28 @@ public class Algorithms {
     public List<Place> scoreDestinations(List<Place> places, int costScore, int accessibilityScore, int popularityScore) {
 
         // Implement scoring logic here to assign a fitness score to each place based on objective functions
+
         // Modify the Place objects in the 'places' list with the computed scores
 
         // Example scoring algorithm:
+
         return places;
     }
+
+    public List<Place> geneticAlgorithm(List<Place> places) {
+        //proceed with scoredDestination places
+        //perform genetic algorithm steps
+
+        //for every generation run scoreDestination ()
+        //after generation finishes then make 50% children
+        //delete 3 (cross-over approach)
+        //keeps fittest population
+        //
+
+        return places;
+    }
+
+
 
     public List<Place> top5(List<Place> places) {
         List<Place> newPlaces = places.subList(0,5);

a5fae62812efdcc59294a33fb95bdfdedac06e7c
Sudanrai88 <101595305+Sudanrai88@users.noreply.github.com>
2023-08-07 17:40:02 +0100

    Fixed fetchItinerariesForUser, now fetches the required data from the firestore database to display on the frontend. Created new ItineraryRequest Class for ease of receiving POST methods from front end.

---
+++ src/main/java/com/example/demo/model/ItineraryRequest.java
@@ -0,0 +1,24 @@
+package com.example.demo.model;
+
+public class ItineraryRequest {
+    private String itineraryId;
+    private String itineraryName;
+
+    public String getItineraryId() {
+        return itineraryId;
+    }
+
+    public void setItineraryId(String itineraryId) {
+        this.itineraryId = itineraryId;
+    }
+
+    public String getItineraryName() {
+        return itineraryName;
+    }
+
+    public void setItineraryName(String itineraryName) {
+        this.itineraryName = itineraryName;
+    }
+
+    // getters and setters
+}
